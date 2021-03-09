0cd61cab09119e1491887896f62b93d9c19c92ec
Tony Narlock <tony@git-pull.com>
2021-03-09 07:59:24 -0600

    ckeditor5-build-eduflow: next-5: Try to replace paragraph (pilcow) icon with add

--- packages/ckeditor5-build-eduflow/webpack.config.js
+++ packages/ckeditor5-build-eduflow/webpack.config.js
@@ -45,6 +45,8 @@ module.exports = {
 	},
 
 	plugins: [
+    new webpack.NormalModuleReplacementPlugin(/pilcrow\.svg/,  '../../ckeditor5-build-eduflow/theme/icons/add.svg'),
+    // new webpack.NormalModuleReplacementPlugin(/pilcrow\.svg/,  '../../../../theme/icons/add.svg'),
 		new CKEditorWebpackPlugin( {
 			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
 			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
@@ -54,7 +56,7 @@ module.exports = {
 		new webpack.BannerPlugin( {
 			banner: bundler.getLicenseBanner(),
 			raw: true
-		} )
+		} ),
 	],
 
 	module: {
